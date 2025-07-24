677249fd7e5d4aa1451d2985147214f2c6a02194
mdeadwiler <mdeadwiler85@gmail.com>
2025-07-23 19:26:37 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/screens/auth/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
-function HomeScreen() {
+function FeedScreen() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to StreetTalk!</Text>
+      <Text>Street Talk</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,8 +18,9 @@ function HomeScreen() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Feed" component={FeedScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
