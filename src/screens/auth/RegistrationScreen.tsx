3fa58859f259df2c22a8d5615277e56fc21a5bb9
mdeadwiler <mdeadwiler85@gmail.com>
2025-07-26 19:33:30 -0700

    Update RegistrationScreen.tsx
    created a paramList for user login with useStates included

--- src/screens/auth/RegistrationScreen.tsx
+++ src/screens/auth/RegistrationScreen.tsx
@@ -4,3 +4,17 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert} from 'react
 import { NativeStackScreenProps } from '@react-navigation/native-stack' 
 import { createUserWithEmailAndPassword} from 'firebase/auth'
 import { auth } from '../../utils/firebaseConfig'
+
+type AuthStackParamList = {
+    Login: { userId: string},
+    Register: { userId: string},
+    Feed: { userId: string},
+}
+
+type RegisterScreenProps = NativeStackScreenProps<AuthStackParamList, 'Register'>
+
+export default function RegisterScreen({ navigation}: RegisterScreenProps ){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+}
