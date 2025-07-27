d4c10cb6d50a198ce1595fb45ef18a45786ad349
mdeadwiler <mdeadwiler85@gmail.com>
2025-07-26 19:49:53 -0700

    Update RegistrationScreen.tsk

--- src/screens/auth/RegistrationScreen.tsx
+++ src/screens/auth/RegistrationScreen.tsx
@@ -17,4 +17,20 @@ export default function RegisterScreen({ navigation}: RegisterScreenProps ){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+
+    const handleLogin = async (): Promise<void> => {
+        if (!email || !password || !confirmPassword) {
+            Alert.alert('Error', 'Please fill in all fields')
+            return
+        }
+
+        if (!email.includes('@')) {
+            Alert.alert('Error', 'Please enter a valid email address')
+            return
+        }
+        if (password !== confirmPassword) {
+            Alert.alert('Error', 'Passwords do not match')
+            return
+        }
+    }
 }
