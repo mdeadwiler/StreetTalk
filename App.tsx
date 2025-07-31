204c6f6ef7a1c4cfbde7e08acbced0549a8086ad
mdeadwiler <mdeadwiler85@gmail.com>
2025-07-30 19:01:58 -0700

    Update App.tsx
     App.tsx had two FeedScreen components overriding each other.
    
    So I fixed this issue by removing local FeedScreen function from App.tsx
    
    Then I imported FeedScreen from ./src/screens/feed/FeedScreen
    
    Then cleared out the unused imports
    
    Result: Now when you login, you'll see your complete FeedScreen.
    
    I went ahead and added a header, sample posts, logout button.
    
    Next steps is creating the screens/PostScreen.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider} from './src/context/AuthContext'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/screens/auth/LoginScreen';
 import RegisterScreen from './src/screens/auth/RegisterScreen';
+import FeedScreen from './src/screens/feed/FeedScreen';
 import { RootStackParamList } from './src/types';
-
-
 
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function FeedScreen({ route }: { route: { params: { userId: string } } }) {
-  const { userId } = route.params;
-  
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Street Talk</Text>
-      <Text style={styles.userId}>Name: {userId}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
 
 export default function App() {
   return (
@@ -38,16 +25,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  userId: {
-    color: '#fffaf0',
-    fontSize: 14,
-    marginTop: 10,
-  },
-});
+
